a6f7e9dd00a8bde2811fc66d71c22967770547a4
vivek <38418771+vivekjava@users.noreply.github.com>
2022-07-19 18:45:53 +0530

    Update gilded_rose.js

--- js-jasmine/src/gilded_rose.js
+++ js-jasmine/src/gilded_rose.js
@@ -1,5 +1,9 @@
+/**
+ *  GitHub source Path : https://github.com/emilybache/GildedRose-Refactoring-Kata/blob/main/js-jasmine/src/gilded_rose.js
+ */
+
 class Item {
-  constructor(name, sellIn, quality){
+  constructor(name, sellIn, quality) {
     this.name = name;
     this.sellIn = sellIn;
     this.quality = quality;
@@ -7,29 +11,32 @@ class Item {
 }
 
 class Shop {
-  constructor(items=[]){
+  constructor(items = []) {
     this.items = items;
   }
   updateQuality() {
-    for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+    for (let i = 0; i < this.items.length; i++) {
+      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert' && this.items[i].name != 'Conjured') {
         if (this.items[i].quality > 0) {
           if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
+            this.items[i].quality = updateQuality(this.items[i], notmalQualityConst, 'REDUCE');
           }
         }
+      } else if (this.items[i].name != 'Conjured') {
+        // New task has been added here
+        this.items[i].quality = updateQuality(this.items[i], notmalQualityConst, 'TWISE_REDUCE')
       } else {
         if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
+          this.items[i].quality = updateQuality(this.items[i], notmalQualityConst, 'INCREMENT');
           if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
+                this.items[i].quality = updateQuality(this.items[i], notmalQualityConst, 'INCREMENT');
               }
             }
             if (this.items[i].sellIn < 6) {
               if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
+                this.items[i].quality = updateQuality(this.items[i], notmalQualityConst, 'INCREMENT');
               }
             }
           }
@@ -43,15 +50,15 @@ class Shop {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].quality > 0) {
               if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
+                this.items[i].quality = updateQuality(this.items[i], notmalQualityConst, 'REDUCE');
               }
             }
           } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
+            this.items[i].quality = updateQuality(this.items[i], notmalQualityConst, 'SELF_REDUCE');
           }
         } else {
           if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
+            this.items[i].quality = updateQuality(this.items[i], notmalQualityConst, 'INCREMENT');
           }
         }
       }
@@ -59,7 +66,25 @@ class Shop {
 
     return this.items;
   }
+  /**
+   * @description this function keep the quality value changes in place
+   * 
+   * @param {*} item 
+   * @param {*} notmalQualityConst 
+   * @param {*} flag 
+   * @returns updateQuality
+   */
+  updateQuality(item, notmalQualityConst, flag = '') {
+    switch (flag) {
+      case 'REDUCE': return (item.quality - notmalQualityConst); break;
+      case 'SELF_REDUCE': return (item.quality - item.quality); break;
+      case 'TWISE_REDUCE': return (item.quality - (notmalQualityConst * 2)); break;
+      case 'INCREMENT': return (item.quality + notmalQualityConst); break;
+      default: return (item.quality + notmalQualityConst); break;
+    }
+  }
 }
+
 module.exports = {
   Item,
   Shop
